d1e1afb88aeef7257949d43d3648a970e7746857
kdoxsey <kdoxse@gmail.com>
2020-10-15 22:13:55 -0400

    Sign up and Sign in done! Change pw still hidden

--- curl-scripts/auth/sign-up.sh
+++ curl-scripts/auth/sign-up.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-curl "https://tic-tac-toe-api-development.herokuapp.com/sign-up" \
+API="https://tic-tac-toe-api-development.herokuapp.com"
+URL_PATH="/sign-in"
+
+curl "${API}${URL_PATH}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
